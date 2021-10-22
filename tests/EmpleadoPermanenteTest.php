a79656abf18ddf0f0fe618f678dbd29a9b52ee10
nato <kevinnatanaelahumada@hotmail.com>
2021-10-22 19:21:44 -0300

    Tests de Empleado Permanente.

--- tests/EmpleadoPermanenteTest.php
+++ tests/EmpleadoPermanenteTest.php
@@ -3,9 +3,48 @@ require_once 'EmpleadoTest.php';
 
 class EmpleadoPermanenteTest extends EmpleadoTest
 {
-    public function crear($nombre = "Taylor", $apellido = "Swift", $dni = "28456133", $salario = 100000, \DateTime $fechaIngreso = null)
+    public function crear($nombre = "Taylor", $apellido = "Swift", $dni = "28456133", $salario = "100000", $fechaIngreso = null)
     {
         $ee = new \App\EmpleadoPermanente($nombre, $apellido, $dni, $salario, $fechaIngreso);
         return $ee;
     }
+
+    public function testSePuedeCrearYObtenerFechaIngreso() {
+        $d = new DateTime('2006-06-19');
+        $e = $this->crear("Kevin", "Ahumada", "12345678", "50000", $d);
+        $this->assertEquals('2006-06-19', $e->getFechaIngreso()->format("Y-m-d"));
+    }
+
+    public function testSePuedeCrearYCalcularComision() {
+        $d = new DateTime('2006-06-19');
+        $e = $this->crear("Kevin", "Ahumada", "12345678", "50000", $d);
+        $this->assertEquals("15%", $e->calcularComision());
+    }
+
+    public function testSePuedeCrearYCalcularIngresoTotal(){
+      $d = new DateTime('2006-06-19');
+      $e = $this->crear("Kevin", "Ahumada", "12345678", "50000", $d);
+      $this->assertEquals("57500", $e->calcularIngresoTotal());
+    }
+
+    public function testSePuedeCrearYCalcularAntiguedad(){
+      $d = new DateTime('2006-06-19');
+      $e = $this->crear("Kevin", "Ahumada", "12345678", "50000", $d);
+      $this->assertEquals("15", $e->calcularAntiguedad());
+    }
+
+    public function testSePuedeCrearSinFechaIngreso() {
+        $e = $this->crear("Kevin", "Ahumada", "12345678", "50000");
+        $today = date("Y-m-d");
+        $this->assertEquals($today, $e->getFechaIngreso()->format("Y-m-d"));
+        $this->assertEquals("0", $e->calcularAntiguedad());
+    }
+
+    public function testNoSePuedeCrearConFechaIngresoFutura(){
+      $this->expectException(\Exception::class);
+      $d = new DateTime('2022-01-01');
+      $e = $this->crear("Kevin", "Ahumada", "12345678", "50000", $d);
+    }
+
+
 }

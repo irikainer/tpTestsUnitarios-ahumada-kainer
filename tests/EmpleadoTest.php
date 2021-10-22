3b3f5cb3ad11371831a5d056ab9e57d8622b63b3
Irina Kainer <kainer.irina@gmail.com>
2021-10-22 17:10:19 -0300

    Tests Empleado terminados

--- tests/EmpleadoTest.php
+++ tests/EmpleadoTest.php
@@ -19,12 +19,7 @@ abstract class EmpleadoTest extends \PHPUnit\Framework\TestCase
       $this->assertEquals("100000", $e->getSalario());
     }
 
-    public function testSePuedeCrearYObtenerSector() {
-      $e = $this->crear();
-      $this->assertEquals("No especificado", $e->getSector());
-    }
-
-    public function testSePuedeCrearYSetearSector() {
+    public function testSePuedeCrearYActualizarSector() {
       $e = $this->crear();
       $e->setSector("Comercial");
       $this->assertEquals("Comercial", $e->getSector());
@@ -33,5 +28,35 @@ abstract class EmpleadoTest extends \PHPUnit\Framework\TestCase
     public function testSePuedeCrearYDevolverString() {
       $e = $this->crear();
       $this->assertEquals("Taylor Swift 28456133 100000", $e->__toString());
+    }
+
+    public function testNoSePuedeCrearConNombreVacio() {
+      $this->expectException(\Exception::class);
+      $e = $this->crear("");
+    }
+
+    public function testNoSePuedeCrearConApellidoVacio() {
+      $this->expectException(\Exception::class);
+      $e = $this->crear("Irina", "");
+    }
+
+    public function testNoSePuedeCrearConDniVacio() {
+      $this->expectException(\Exception::class);
+      $e = $this->crear("Irina", "Kainer", "");
+    }
+
+    public function testNoSePuedeCrearConSalarioVacio() {
+      $this->expectException(\Exception::class);
+      $e = $this->crear("Irina", "Kainer", "12345678", "");
+    }
+
+    public function testNoSePuedeCrearConDniInvalido() {
+      $this->expectException(\Exception::class);
+      $e = $this->crear("Irina", "Kainer", "letras--");
+    }
+
+    public function testSePuedeCrearSinSector() {
+      $e = $this->crear();
+      $this->assertEquals("No especificado", $e->getSector());
     }
 }
